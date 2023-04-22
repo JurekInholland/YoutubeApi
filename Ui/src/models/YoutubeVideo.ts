688d208b4d413c4bddc7b0665952ddb1c3a67ac9
Jurek Baumann <jurekbaumann@me.com>
2023-04-22 03:32:31 +0200

    relationship

--- Ui/src/models/YoutubeVideo.ts
+++ Ui/src/models/YoutubeVideo.ts
@@ -13,9 +13,9 @@ export default class YoutubeVideo extends Model {
       description: this.string(''),
       width: this.number(0),
       height: this.number(0),
-      dateAdded: this.number(0),
-      lastUpdated: this.number(0),
-      uploadDate: this.number(0),
+      dateAdded: this.attr(''),
+      lastUpdated: this.attr(''),
+      uploadDate: this.attr(''),
       duration: this.string(''),
       viewCount: this.number(0),
       likeCount: this.number(0),
@@ -23,8 +23,14 @@ export default class YoutubeVideo extends Model {
       relatedVideos: this.attr([]),
       playableInEmbed: this.boolean(false),
 
-
-      youtubeChannel: this.belongsTo(YoutubeChannel, 'id')
+      youtubeChannel: this.belongsTo(YoutubeChannel, 'youtubeChannel')
     }
   }
+//   static casts() {
+//     return {
+//       dateAdded: Date,
+//       lastUpdated: Date,
+//       uploadDate: Date
+//     }
+//   }
 }
--- Ui/src/views/OrmTests.vue
+++ Ui/src/views/OrmTests.vue
@@ -1,29 +1,28 @@
 <script setup lang="ts">
 import { useRepo } from 'pinia-orm'
 import YoutubeVideo from '@/models/YoutubeVideo';
-import { ormService } from '@/constants'
+import { ormService, apiService } from '@/constants'
 const videoRepo = useRepo(YoutubeVideo);
 
 
 const fetchVideo = async () => {
-    const video = await ormService.getVideoById("WFkSKEo3CVw");
-    if (video === null) return;
+    // const video = await ormService.getVideoById("WFkSKEo3CVw");
+    const video = await apiService.GetVideoInfo("WFkSKEo3CVw")
     videoRepo.save(video);
     // console.log(video);
 }
 
 const test = videoRepo.with('youtubeChannel').all();
-console.log(test);
+console.log(videoRepo.piniaStore());
 </script>
 
 <template>
-
-<button @click="fetchVideo">Fetch Video</button>
-<div v-if="videoRepo.with('youtubeChannel').all().length > 0" v-for="video of videoRepo.with('youtubeChannel').all()">
-    {{ video.title }}
-    {{ video.id }}
-    <!-- {{ video.youtubeChannel.title }} -->
-</div>
+    <button @click="fetchVideo">Fetch Video</button>
+    <div v-if="videoRepo.with('youtubeChannel').all().length > 0" v-for="video of videoRepo.with('youtubeChannel').all()">
+        {{ video.title }}
+        {{ video.id }}
+        <!-- {{ video.youtubeChannel.title }} -->
+    </div>
 </template>
 
 <style scoped>
