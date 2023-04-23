472cbbcbfbec9317df729bb81bc17e3bbb502139
Jurek Baumann <jurekbaumann@me.com>
2023-04-24 01:59:52 +0200

    orm video

---
+++ Ui/src/views/OrmVideoPage.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const parsedId = computed(() => {
+  const substring = route.path.substring(1);
+  if (substring.length === 11) return substring
+  const pathId = substring.split("/")[1]
+  if (pathId !== undefined && pathId.length === 11) return pathId
+  return route.query.v ? route.query.v as string : null
+})
+
+
+</script>
+<template>
+    <div>
+
+        <p>Video: {{ }}</p>
+
+    </div>
+</template>
